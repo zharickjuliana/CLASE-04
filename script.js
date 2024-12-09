23f34d9c655e2b4e7f11bb5233dcd6033e3e7285
zharickjuliana <zharick141104@gmail.com>
2024-12-09 16:55:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-function realizarOperación(operación) {
+function realizarOperacion(operacion) {
     let num1 = parseFloat(document.getElementById("num1").value);
     let num2 = parseFloat(document.getElementById("num2").value);
-
-    // validar entrada de números
-    if(isNaN(num1) || isNaN(num2)){
-        document.getElementById("result").innerText = "por favor, ingrese números válidos"
-        return;
+  
+    // Validar entrada de números
+    if (isNaN(num1) || isNaN(num2)) {
+      document.getElementById("result").innerText = "Por favor, ingrese números válidos.";
+      return;
     }
-let resultado;
-if (operación === "suma"){
-       resultado = numn1 + num2;
-    } else if (operación === "resta"){
-       resultado = num1 - num2;
-    } else if (operación === "multiplicación"){
-       resultado = num1 * num2;      
-    } else if (operación === "división"){
-       if (num2 === 0) {
-        resultado =" error: no se puede dividir entre cero.";
-     } else {
+  
+    let resultado;
+    if (operacion === "suma") {
+      resultado = num1 + num2;
+    } else if (operacion === "resta") {
+      resultado = num1 - num2;
+    } else if (operacion === "multiplicacion") {
+      resultado = num1 * num2;
+    } else if (operacion === "division") {
+      if (num2 === 0) {
+        resultado = "Error: No se puede dividir entre cero.";
+      } else {
         resultado = num1 / num2;
-     }
-} else {
-    resultado = "Error: operación no válida.";
-}
-
-
-// Mostrar resultado
-document.getElementById("result").innerText = "Resultado: " + resultado; 
-}
+      }
+    } else {
+      resultado = "Error: Operación no válida.";
+    }
+  
+    // Mostrar resultado
+    document.getElementById("result").innerText = "Resultado: " + resultado;
+  }
+  
